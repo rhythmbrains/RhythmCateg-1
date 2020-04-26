92cf30dc6c6d3c90dcc1ef88fd6cd48f9fd0b2e5
Ceren Battal <battal.ceren@gmail.com>
2020-04-26 15:57:15 +0200

    Update

--- tapMainExperiment.m
+++ tapMainExperiment.m
@@ -1,6 +1,16 @@
 
+% Clear all the previous stuff
+% clc; clear;
+if ~ismac
+    close all;
+    clear Screen;
+end
 
-%%
+% make sure we got access to all the required functions and inputs
+addpath(genpath(fullfile(pwd, 'lib')))
+
+% Get parameters
+[cfg,expParameters] = getParams();
 
 % set and load all the parameters to run the experiment
 [subjectName, runNumber] = getSubjectID(cfg);
@@ -11,11 +21,38 @@
 % Safety loop: close the screen if code crashes
 try
     %% Init the experiment
-    [cfg] = initPTB(cfg);
+    [cfg] = initPTB(device,cfg);
+    
+    
+    
+    % Empty vectors and matrices for speed
+    % logFile.xx = [];
     % Prepare for the output logfiles
     logFile = saveOutput(subjectName, logFile, ExpParameters, 'open');
 
-%%
+
+    % Show instructions
+    if ExpParameters.Task1
+        DrawFormattedText(Cfg.win,ExpParameters.TaskInstruction,...
+            'center', 'center', Cfg.textColor);
+        Screen('Flip', Cfg.win);
+    end
+    
+    %  instructions
+    displayInstr(expParameters.taskInstruction,cfg.screen,cfg.keywait);
+    
+    displayInstr('TAP',cfg.screen);
+    
+    
+    
+    
+    
+    
+%% make stimuli
+
+%% fill the buffer
+
+%% start playing
 
 % sound repetition
 repetitions = 1;
@@ -32,14 +69,15 @@ pahandle = cfg.pahande;
 % start the sound sequence
 playTime = PsychPortAudio('Start', pahandle, repetitions, startCue, waitForDeviceStart);
 
-
 %save the time to cfg
 cfg.playTime = playTime;
 
+%%
+cleanUp()
 
 catch
     
-    %cleanUp()
-    %psychrethrow(psychlasterror);
+    cleanUp()
+    psychrethrow(psychlasterror);
     
 end
