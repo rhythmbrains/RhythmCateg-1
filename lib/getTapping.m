d0726ff46143de15e49087fd628049f37d84f1d6
Ceren Battal <battal.ceren@gmail.com>
2020-05-06 17:17:55 +0200

    evolving response collection function

--- lib/getTapping.m
+++ lib/getTapping.m
@@ -1,10 +1,17 @@
-function getTapping(action, cfg)
+function responseEvents = getTapping(action, cfg)
 % wrapper function to use KbQueue
 
 % getTapping(action, cfg, expParameters)
 
 
 responseBox = cfg.responseBox;
+
+responseEvents = struct;
+responseEvents.onset = [];
+responseEvents.trial_type = [];
+responseEvents.duration = [];
+responseEvents.key_name = [];
+responseEvents.pressed = [];
 
 
 switch action
@@ -24,12 +31,32 @@ switch action
         
     case 'collect'
         
-%         while KbEventAvail(responseBox)
-%             
-%             event = KbEventGet(responseBox);
-%             
-%             responseEvents(end+1, :) = [event.Time event.Keycode event.Pressed];  %#ok<*AGROW>
-%         end
+        
+        if verbose
+            fprintf('\n checking recent keypresses\n')
+        end
+        
+        iEvent = 1;
+         
+        while KbEventAvail(responseBox)
+            
+            event = KbEventGet(responseBox);
+            
+            % we only return the pressed keys by default
+            if getOnlyPress && event.Pressed==0
+            else
+                
+                responseEvents(iEvent,1).onset = event.Time;
+                responseEvents(iEvent,1).trial_type = 'response';
+                responseEvents(iEvent,1).duration = 0;
+                responseEvents(iEvent,1).key_name = KbName(event.Keycode);
+                responseEvents(iEvent,1).pressed =  event.Pressed;
+
+            end
+            
+            iEvent = iEvent + 1;
+            
+        end
         
         
     case 'flush'
