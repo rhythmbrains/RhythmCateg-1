66e0b25f35ff7361c1a03241bbea2b8f5759188e
Ceren <battal.ceren@gmail.com>
2020-07-21 19:02:09 +0200

    bids files add

--- lib/CPP_BIDS/tests/test_createFilename.m
+++ lib/CPP_BIDS/tests/test_createFilename.m
@@ -1,87 +1,111 @@
 function test_createFilename()
-% test for filename creation and their directories
+    % test for filename creation and their directories
 
-%% check directory and filename creation (PC)
+    %% PC
 
-%%% set up part
+    outputDir = fullfile(fileparts(mfilename('fullpath')), '..', 'output');
 
-expParameters.subjectGrp = '';
-expParameters.subjectNb = 1;
-expParameters.sessionNb = 1;
-expParameters.runNb = 1;
-expParameters.task = 'testtask';
-expParameters.verbose = 1;
+    %%% set up part
 
-cfg.eyeTracker = false;
-cfg.device = 'PC';
+    expParameters.subjectGrp = '';
+    expParameters.subjectNb = 1;
+    expParameters.sessionNb = 1;
+    expParameters.runNb = 1;
+    expParameters.task = 'testtask';
+    expParameters.outputDir = outputDir;
 
-% set up the output directories
-outputDir = fullfile(fileparts(mfilename('fullpath')), '..', 'output');
-expParameters.outputDir = outputDir;
+    cfg = struct();
 
-behDir = fullfile(outputDir, 'source', 'sub-001', 'ses-001', 'beh');
-eyetrackerDir = fullfile(outputDir, 'source', 'sub-001', 'ses-001', 'eyetracker');
+    %%% run part
+    [cfg, expParameters] = createFilename(cfg, expParameters);
 
-expParameters = checkCFG(cfg,expParameters);
-expParameters = createFilename(cfg,expParameters);
+    %%% test part
 
-%%% test part
+    % test data
+    behDir = fullfile(outputDir, 'source', 'sub-001', 'ses-001', 'beh');
+    eyetrackerDir = fullfile(outputDir, 'source', 'sub-001', 'ses-001', 'eyetracker');
+    eventFilename = ['sub-001_ses-001_task-testtask_run-001_events_date-'...
+        expParameters.date '.tsv'];
+    stimFilename =  ['sub-001_ses-001_task-testtask_run-001_stim_date-'...
+        expParameters.date '.tsv'];
 
-% make sure the beh dir is created
-assert(exist(behDir, 'dir')==7)
+    % make sure the beh dir is created
+    assert(exist(behDir, 'dir') == 7);
 
-% make sure the eyetracker dir is not created
-assert(exist(eyetrackerDir, 'dir')==0)
+    % make sure the eyetracker dir is not created
+    assert(exist(eyetrackerDir, 'dir') == 0);
 
-% make sure the events filename is created
-assert(strcmp(...
-    expParameters.fileName.events, ...
-    ['sub-001_ses-001_task-testtask_run-001_events_date-' expParameters.date '.tsv']));
+    % make sure the events filename is created
+    assert(strcmp(expParameters.fileName.events, eventFilename));
 
-% make sure the stim filename is created
-assert(strcmp(...
-    expParameters.fileName.stim, ...
-    ['sub-001_ses-001_task-testtask_run-001_stim_date-' expParameters.date '.tsv']));
+    % make sure the stim filename is created
+    assert(strcmp(expParameters.fileName.stim, stimFilename));
 
-%% check directory and filename creation (fMRI and eye tracker)
+    %% fMRI and eye tracker
 
-clear
+    clear;
 
-%%% set up part
+    outputDir = fullfile(fileparts(mfilename('fullpath')), '..', 'output');
 
-expParameters.subjectGrp = 'ctrl';
-expParameters.subjectNb = 2;
-expParameters.sessionNb = 2;
-expParameters.runNb = 2;
-expParameters.task = 'testtask';
+    %%% set up part
 
-cfg.eyeTracker = true;
-cfg.device = 'scanner';
+    expParameters.subjectGrp = 'ctrl';
+    expParameters.subjectNb = 2;
+    expParameters.sessionNb = 2;
+    expParameters.runNb = 2;
+    expParameters.task = 'testtask';
+    expParameters.outputDir = outputDir;
 
-outputDir = fullfile(fileparts(mfilename('fullpath')), '..', 'output');
+    cfg.eyeTracker = true;
+    cfg.testingDevice = 'mri';
 
-funcDir = fullfile(outputDir, 'source', 'sub-ctrl002', 'ses-002', 'func');
-eyetrackerDir = fullfile(outputDir, 'source', 'sub-ctrl002', 'ses-002', 'eyetracker');
+    [cfg, expParameters] = createFilename(cfg, expParameters);
 
-expParameters.outputDir = outputDir;
-expParameters = checkCFG(cfg,expParameters);
-expParameters = createFilename(cfg,expParameters);
+    %%% test part
 
-%%% test part
+    % test data
+    funcDir = fullfile(outputDir, 'source', 'sub-ctrl002', 'ses-002', 'func');
+    eyetrackerDir = fullfile(outputDir, 'source', 'sub-ctrl002', 'ses-002', 'eyetracker');
+    baseFilename = 'sub-ctrl002_ses-002_task-testtask';
+    eventFilename = ['sub-ctrl002_ses-002_task-testtask_run-002_events_date-' ...
+        expParameters.date '.tsv'];
+    eyetrackerFilename =  ['sub-ctrl002_ses-002_task-testtask_run-002_eyetrack_date-' ...
+        expParameters.date '.edf'];
 
-% make sure the func dir is created
-assert(exist(funcDir, 'dir')==7)
+    % make sure the func dir is created
+    assert(exist(funcDir, 'dir') == 7);
 
-% make sure the eyetracker dir is created
-assert(exist(eyetrackerDir, 'dir')==7)
+    % make sure the eyetracker dir is created
+    assert(exist(eyetrackerDir, 'dir') == 7);
 
-% make sure the events filename is created
-assert(strcmp(expParameters.fileName.base, 'sub-ctrl002_ses-002_task-testtask'))
-assert(strcmp(...
-    expParameters.fileName.events, ...
-    ['sub-ctrl002_ses-002_task-testtask_run-002_events_date-' expParameters.date '.tsv']));
+    % make sure the right filenames are created
+    assert(strcmp(expParameters.fileName.base, baseFilename));
+    assert(strcmp(expParameters.fileName.events, eventFilename));
+    assert(strcmp(expParameters.fileName.eyetracker, eyetrackerFilename));
 
-% make sure the eyetracker filename is created
-assert(strcmp(...
-    expParameters.fileName.eyetracker, ...
-    ['sub-ctrl002_ses-002_task-testtask_run-002_eyetrack_date-' expParameters.date '.edf']));
+    %% EEG
+
+    clear;
+
+    outputDir = fullfile(fileparts(mfilename('fullpath')), '..', 'output');
+
+    %%% set up part
+
+    expParameters.subjectGrp = 'blind';
+    expParameters.subjectNb = 3;
+    expParameters.sessionNb = 1;
+    expParameters.runNb = 1;
+    expParameters.task = 'testtask';
+    expParameters.outputDir = outputDir;
+
+    cfg.testingDevice = 'eeg';
+
+    [cfg, expParameters] = createFilename(cfg, expParameters); %#ok<ASGLU>
+
+    %%% test part
+
+    % test data
+    eegDir = fullfile(outputDir, 'source', 'sub-blind003', 'ses-001', 'eeg');
+
+    % make sure the func dir is created
+    assert(exist(eegDir, 'dir') == 7);
--- lib/CPP_BIDS/userInputs.m
+++ lib/CPP_BIDS/userInputs.m
@@ -1,71 +1,74 @@
 function [expParameters] = userInputs(cfg, expParameters)
-% Get subject, run and session number and make sure they are
-% positive integer values
-%
-% expParameters.askGrpSess
-% a 1 X 2 array of booleans (default is [true true] ):
-%    - the first value set to false will skip asking for the participants
-%    group
-%    - the second value set to false will skip asking for the session
+    % Get subject, run and session number and make sure they are
+    % positive integer values
+    %
+    % expParameters.askGrpSess
+    % a 1 X 2 array of booleans (default is [true true] ):
+    %    - the first value set to false will skip asking for the participants
+    %    group
+    %    - the second value set to false will skip asking for the session
 
-if nargin<1 || isempty(cfg.debug)
-    cfg.debug = false;
-end
-if nargin<2
-    expParameters = [];
-end
-if ~isfield(expParameters, 'askGrpSess') || isempty(expParameters.askGrpSess)
+    if nargin < 1 || isempty(cfg.debug)
+        cfg.debug = false;
+    end
+    if nargin < 2
+        expParameters = [];
+    end
+    
     askGrpSess = [true true];
-else
-    askGrpSess = expParameters.askGrpSess;
-end
-
-
-% When in debug more this function returns some dummy values
-if cfg.debug
-    subjectGrp = 'ctrl';
-    subjectNb = 666;
-    runNb = 666;
-    sessionNb = 666;
+    if isfield(expParameters, 'askGrpSess') && ~isempty(expParameters.askGrpSess)
+        askGrpSess = expParameters.askGrpSess;
+    end
+    if numel(askGrpSess) < 2
+        askGrpSess(2) = 1;
+    end
     
-    % Otherwise it prompts the user for some information
-else
-    
-    % subject group
-    if askGrpSess(1)
-        subjectGrp = lower(input('Enter subject group (leave empty if none): ', 's'));
-    else
+    subjectGrp = '';
+    subjectNb = []; %#ok<*NASGU>
+    sessionNb = [];
+    runNb = [];
+
+    % When in debug more this function returns some dummy values
+    if cfg.debug
         subjectGrp = 'ctrl';
-    end
-    
-    % the subject number
-    subjectNb = str2double(input('Enter subject number (1-999): ', 's') );
-    subjectNb = checkInput(subjectNb);
-    
-    % the session number
-    if  numel(askGrpSess)>1 && askGrpSess(2)
-        sessionNb = str2double(input('Enter the session (i.e day - 1-999)) number: ', 's'));
-        sessionNb = checkInput(sessionNb);
-    else
+        subjectNb = 666;
+        runNb = 666;
         sessionNb = 666;
-    end
-    
-    % the run number
-    runNb = str2double(input('Enter the run number (1-999): ', 's'));
-    runNb = checkInput(runNb);
-    
-end
 
-expParameters.subjectGrp = subjectGrp;
-expParameters.subjectNb = subjectNb;
-expParameters.sessionNb = sessionNb;
-expParameters.runNb = runNb;
+        % Otherwise it prompts the user for some information
+    else
+
+        % subject group
+        if askGrpSess(1)
+            subjectGrp = lower(input('Enter subject group (leave empty if none): ', 's'));
+        end
+
+        % the subject number
+        subjectNb = str2double(input('Enter subject number (1-999): ', 's'));
+        subjectNb = checkInput(subjectNb);
+
+        % the session number
+        if  numel(askGrpSess) > 1 && askGrpSess(2)
+            sessionNb = str2double(input('Enter the session (i.e day - 1-999)) number: ', 's'));
+            sessionNb = checkInput(sessionNb);
+        end
+
+        % the run number
+        runNb = str2double(input('Enter the run number (1-999): ', 's'));
+        runNb = checkInput(runNb);
+
+    end
+
+    expParameters.subjectGrp = subjectGrp;
+    expParameters.subjectNb = subjectNb;
+    expParameters.sessionNb = sessionNb;
+    expParameters.runNb = runNb;
 
 end
 
 function input2check = checkInput(input2check)
-% this function checks the input to makes sure the user enters a positive integer
-while isnan(input2check) || fix(input2check) ~= input2check || input2check<0
-    input2check = str2double(input('Please enter a positive integer: ', 's'));
-end
+    % this function checks the input to makes sure the user enters a positive integer
+    while isnan(input2check) || fix(input2check) ~= input2check || input2check < 0
+        input2check = str2double(input('Please enter a positive integer: ', 's'));
+    end
 end
