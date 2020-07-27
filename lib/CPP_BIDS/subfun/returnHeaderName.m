23be993081771726c889f0f5deb790ad206ee821
Ceren <battal.ceren@gmail.com>
2020-07-27 19:14:41 +0100

    removing bids from lib RnB folder

--- lib/CPP_BIDS/subfun/returnHeaderName.m
+++
@@ -1,7 +0,0 @@
-function headerName = returnHeaderName(columnName, nbCol, iCol)
-    if nbCol == 1
-        headerName = sprintf('%s', columnName);
-    else
-        headerName = sprintf('%s_%02.0f', columnName, iCol);
-    end
-end
--- lib/CPP_BIDS/subfun/returnNamesExtraColumns.m
+++
@@ -1,9 +0,0 @@
-function [namesExtraColumns, logFile] = returnNamesExtraColumns(logFile)
-
-    namesExtraColumns = [];
-
-    if isfield(logFile, 'extraColumns') && ~isempty(logFile(1).extraColumns)
-        namesExtraColumns = fieldnames(logFile(1).extraColumns);
-    end
-
-end
